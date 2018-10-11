03b720bdbc54c345ac86793d307dd29fc6872fb6
HauntedHeart <1091881456@qq.com>
2018-10-11 12:09:52 +0800

    hellworld

---
+++ app/src/main/java/cn/edu/pku/yangguanbin/yangguanbin/bean/City.java
@@ -0,0 +1,34 @@
+package cn.edu.pku.yangguanbin.yangguanbin.bean;
+
+/**
+ * Created by yangg on 2018/10/4.
+ */
+
+public class City {/*
+    private String province;
+    private String city;
+    private String number;
+    private String firstPY;
+    private String allPY;
+    private String allFristPY;
+
+    public City(String province, String city, String number,
+                String firstPY, String allPY, String allFristPY) {
+        this.province = province;
+        this.city = city;
+        this.number = number;
+        this.firstPY = firstPY;
+        this.allPY = allPY;
+        this.allFristPY = allFristPY;
+    }
+*/
+    /*public String getProvince() {return  province;}
+    public String getCity(){return city;}
+    public String getNumber(){return number;}
+    public String  getFirstPY(){return firstPY;}
+    public String   getAllPY(){return allPY;}
+    public String  getAllFristPY(){return allFristPY;}
+    */
+
+
+}
